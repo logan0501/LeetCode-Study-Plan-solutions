99861f24f59d90cbd3f30bef843118b17eca7c54
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-01-21 13:25:56 +0530

    Time: 235 ms (5.07%), Space: 55.2 MB (8.22%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& v) {
-        unordered_set<int> uset;
-        for(int i=0;i<v.size();i++){
-            if(uset.find(v[i])!=uset.end()){
-                return true;
-            }
-            uset.insert(v[i]);
-        }
-        return false;
+        unordered_set<int> uset(v.begin(),v.end());
+        return uset.size()!=v.size();
     }
 };
