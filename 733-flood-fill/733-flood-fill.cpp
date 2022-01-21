e27cbf7df26f21dd89bd7a2efdd827899357fdeb
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-01-21 22:14:51 +0530

    Time: 12 ms (51.25%), Space: 14 MB (81.26%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
+    void dfs(int i,int j,vector<vector<int>> &img,int color,int paint){
+        if(i<0 || j<0 || i>=img.size() || j>=img[0].size() || img[i][j]!=color) return;
+        img[i][j]=paint;
+        dfs(i+1,j,img,color,paint);
+        dfs(i,j+1,img,color,paint);
+        dfs(i-1,j,img,color,paint);
+        dfs(i,j-1,img,color,paint);
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-              if(image[sr][sc] == newColor)
-            return image;
-        int k = image[sr][sc];
-        image[sr][sc] = newColor;
-         if((sr-1) >=0 && image[sr-1][sc] == k)
-             image = floodFill(image,sr-1,sc,newColor);
-          if((sc-1) >=0 && image[sr][sc-1] == k)
-             image = floodFill(image,sr,sc-1,newColor);
-         if((sr+1) < image.size() && image[sr+1][sc] == k)
-             image = floodFill(image,sr+1,sc,newColor);
-          if((sc+1) < image[0].size() && image[sr][sc+1] == k)
-             image = floodFill(image,sr,sc+1,newColor);
-        
-        return image;  
+        if(image.size() && image[sr][sc]!=newColor)dfs(sr,sc,image,image[sr][sc],newColor);
+        return image;
     }
 };
