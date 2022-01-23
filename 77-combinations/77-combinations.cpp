9f55464af03dc3f67d9225d84ef1ab002723a49f
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-01-24 00:06:26 +0530

    Time: 424 ms (19.23%), Space: 164.1 MB (18.06%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-	vector<vector<int>> combine(int n, int k) {
-		vector<vector<int>> result;
-		int i = 0;
-		vector<int> p(k, 0);
-		while (i >= 0) {
-			p[i]++;
-			if (p[i] > n) --i;
-			else if (i == k - 1) result.push_back(p);
-			else {
-			    ++i;
-			    p[i] = p[i - 1];
-			}
-		}
-		return result;
-	}
+vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> res;
+    vector<int> temp;
+    backtrack(1,k,res,temp,n);
+    return res;
+}
+void backtrack(int i,int k,vector<vector<int>> &res,vector<int> temp,int n){
+    if(temp.size()==k){
+        res.push_back(temp);
+        return;
+    }
+    for(int j=i;j<n+1;j++){
+        temp.push_back(j);
+        backtrack(j+1,k,res,temp,n);
+        temp.pop_back();
+    }
+}
 };
