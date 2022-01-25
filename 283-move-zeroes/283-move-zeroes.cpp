2ce88739ee3033e33b57c1448de9ccbb1add24e0
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-01-25 19:55:01 +0530

    Time: 30 ms (46.48%), Space: 19.2 MB (57.23%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -4,9 +4,9 @@ public:
     int j=0;
     for(int i=0;i<arr.size();i++){
         if(arr[i]!=0){
-            swap(arr[j++],arr[i]);
+            swap(arr[i],arr[j++]);
         }
-    }    
+    }
     
     }
 };
