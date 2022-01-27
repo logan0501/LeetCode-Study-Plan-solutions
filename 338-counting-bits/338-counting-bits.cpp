073188123ac68025fca509addbeaa262b4bb175b
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-01-27 08:57:27 +0530

    Time: 10 ms (27.52%), Space: 7.6 MB (98.18%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
-    vector<int> ans;
-    void helper(int i){
-        if(i==0){ans.push_back(0);return;}
-        if(i==1){ans.push_back(0);ans.push_back(1);return;}
-        helper(i-1);
-      
-        if((i&1)==0){
-           
-              ans.push_back(ans[(i>>1)]); 
-        }else {
-           
-            ans.push_back(ans[i>>1]+1);}
-        
-    }
+
     vector<int> countBits(int n) {
-        helper(n);
-        return ans;
+        vector<int> res(n+1,0);
+
+        for(int i=1;i<=n;i++){
+            res[i]=res[(i&(i-1))]+1;
+        }
+        return res;
     }
 };
