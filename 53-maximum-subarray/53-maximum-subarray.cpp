a99c67ad9cea57ccf5046dcb663e17750ba8f85e
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-01-29 21:58:43 +0530

    Time: 170 ms (30.44%), Space: 67.7 MB (53.50%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,6 +4,7 @@ public:
             int maxv=nums[0],res=nums[0];
     for(int i=1;i<nums.size();i++){
        maxv=max(maxv+nums[i],nums[i]);
+      
        res=max(res,maxv);
     }
     return res;
