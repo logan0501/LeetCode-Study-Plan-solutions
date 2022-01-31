bfcb413d53085f53718b1700709994756684dbd6
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-01-31 10:40:40 +0530

    Time: 19 ms (22.17%), Space: 11.4 MB (87.91%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -5,7 +5,7 @@ public:
         while(l<h){
             int mid = l+(h-l)/2;
             if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])return mid;
-            else if(arr[mid]<arr[mid+1])l=mid;
+            else if(arr[mid]<arr[mid+1])l=mid+1;
             else h=mid;
         }
         return l;
