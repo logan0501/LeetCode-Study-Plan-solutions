94549f617eb156bad9e85089bd7fd5425ffbe039
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-02-08 18:20:40 +0530

    Time: 77 ms (82.00%), Space: 20 MB (86.69%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        if(nums.empty())return res;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+          if (nums[i] > 0) break;
+            if (i > 0 and nums[i] == nums[i-1]) continue;
+             int l=i+1,r=n-1;
+            while(l<r){
+                int sum=nums[l]+nums[r]+nums[i];
+                if(sum>0)r--;
+                else if(sum<0)l++;
+                else{
+                    res.push_back({nums[i],nums[l],nums[r]});
+                    int prevl=nums[l],prevr=nums[r];
+                    while(l<r && nums[l]==prevl)l++;
+                    while(l<r && nums[r]==prevr)r--;
+                }
+            }
+        }
+        return res;
+        
+    }
+};
