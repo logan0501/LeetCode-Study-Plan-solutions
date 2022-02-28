39729b571ca4f6a2f2f5425a5c356423c4a80caf
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-02-28 20:54:51 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (14.46%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -4,10 +4,15 @@ public:
         vector<string> res;
         int len = nums.size(), i = 0, j;
         while (i < len) {
-            j = i + 1;
+            try{
+                         j = i + 1;
             while (j < len && nums[j]==nums[j-1] +1) j++;
             res.push_back(to_string(nums[i]) + (j - i > 1 ? "->" + to_string(nums[j - 1]) : ""));
-            i = j;
+            i = j;   
+            }catch(...){
+                cout<<nums[i]<<nums[i-1];
+            }
+
         }
         return res;
     }
