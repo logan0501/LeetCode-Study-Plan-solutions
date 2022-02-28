b6721d7eb474a64a3e2c7ff70998830cd4619754
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-02-28 21:04:25 +0530

    Time: 0 ms (100.00%), Space: 7 MB (14.46%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -6,7 +6,7 @@ public:
         while (i < len) {
             try{
                          j = i + 1;
-            while (j < len && nums[j]==nums[j-1] +1) j++;
+            while (j < len && nums[j]==nums[j-1]+1) j++;
             res.push_back(to_string(nums[i]) + (j - i > 1 ? "->" + to_string(nums[j - 1]) : ""));
             i = j;   
             }catch(...){
