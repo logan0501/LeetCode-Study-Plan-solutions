4d68ed3d545203a1cc42f06fa3d34ab9b77eef01
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-03-08 18:02:17 +0530

    Time: 24 ms (84.99%), Space: 10 MB (48.66%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    
-    void helper(int idx, int k,vector<int>&current,int n)
-    {
-        if(current.size()==k)   
-        {
-            ans.push_back(current);
-            return;
-        }
-        
-        for(int i=idx;i<n+1;i++)
-        {
-            current.push_back(i); 
-            helper(i+1,k,current,n);
-            current.pop_back(); 
-        }
-    }
-    
-    vector<vector<int>> combine(int n, int k) {
-        vector<int>current;
-        helper(1,k,current,n);
-        return ans;
-    }
+vector<vector<int>> ans;
+void helper(int idx,int k,vector<int> &cur,int n){
+	if(cur.size()==k){
+		ans.push_back(cur);return;
+	}
+	for(int i=idx;i<=n;i++){
+		cur.push_back(i);
+		helper(i+1,k,cur,n);
+		cur.pop_back();
+	}
+}
+ vector<vector<int>> combine(int n, int k) {
+ 	vector<int> cur;
+    helper(1,k,cur,n);
+    return ans;
+ }
 };
