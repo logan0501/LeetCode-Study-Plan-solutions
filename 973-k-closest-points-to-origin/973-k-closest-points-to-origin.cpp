77d56d985e6840619720d54bdf1ed41eee31007b
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-03-28 22:52:02 +0530

    Time: 418 ms (34.89%), Space: 85.4 MB (12.56%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& P, int k) {
-        auto euclidean = [](auto& p) { return p[0] * p[0] + p[1] * p[1]; };
-        sort(begin(P), end(P), [&](auto& a, auto& b) { return euclidean(a) < euclidean(b); });
-        P.resize(k);
-        return P;
+vector<vector<int>> kClosest(vector<vector<int>>& P, int k) {
+    priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> heap;
+    vector<vector<int>> res;
+    for(auto &a:P){
+        auto p = a;
+        heap.push({(int)(pow(p[0],2)+pow(p[1],2)),p[0],p[1]});
     }
+    while (k--)
+    {
+        auto top= heap.top();
+        heap.pop();
+        res.push_back({top[1],top[2]});
+    }
+    return res;
+    
+}
 };
