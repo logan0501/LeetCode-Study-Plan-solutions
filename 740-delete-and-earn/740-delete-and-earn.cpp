7d5005d48f6b2d2a5dd457dd68d7bcfbd8974244
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-04-23 07:53:52 +0530

    Time: 0 ms (100.00%), Space: 14.1 MB (17.92%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-vector<int> dp;
-int helper(int i,vector<int> freq){
-    if(i>=freq.size())return 0;
-    if(dp[i]!=-1)return dp[i];
-    return dp[i]=max(helper(i+2,freq)+freq[i],helper(i+1,freq));
-}
-int deleteAndEarn(vector<int> nums) {
-     vector<int> freq(*max_element(nums.begin(),nums.end())+1,0);
-     for(auto &a:nums){freq[a]+=a;}
-     dp.resize(10001,-1);
-     return helper(0,freq);  
-}
+    int deleteAndEarn(vector<int>& nums) {
+        vector<int> dp;
+        vector<int> freq(*max_element(nums.begin(),nums.end())+1,0);
+        for(auto &a:nums){freq[a]+=a;}
+        dp.resize(10001,-1);
+        dp[0]=freq[0];
+        dp[1]=freq[1];
+        for(int i=2;i<freq.size();i++){
+            dp[i]=max(dp[i-1],dp[i-2]+freq[i]);
+        }
+        return dp[freq.size()-1];   
+    }
 };
