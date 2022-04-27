ca72fb58faa24b94f0ceaf3d7e18f329f77bad65
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-04-27 13:55:51 +0530

    Time: 524 ms (57.09%), Space: 57.9 MB (70.38%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,60 +1,36 @@
 class Solution {
 public:
-    
-    int getParent(vector<int>& parents, int idx){
-        
-        if(parents[idx]==idx){
-            return idx;
-        }
-        
-        parents[idx] = getParent(parents, parents[parents[idx]]);
-        
-        return parents[idx];
-        
-        
-    }
-    
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        
-        int n = points.size();
-        vector<int> parents(n);        
-        iota(parents.begin(), parents.end(), 0);
-        int ans = 0;
-        int edges = 0;
-        priority_queue<vector<int>> pq;
+int par[1001];
+int find(int a){
+    if(par[a]<0)return a;
+    return par[a]=find(par[a]);
+}
+void Union(int a,int b){
+    par[a]=b;
+}
+int minCostConnectPoints(vector<vector<int>>& points) {
+    int n=points.size();
+    for(int i=0;i<n;i++)par[i]=-1;
+    vector<pair<int,pair<int,int>>> graph;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            int wt=abs(points[i][0] - points[j][0]) + 
+                             abs(points[i][1] - points[j][1]);
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(i!=j){
-                     pq.push({-1*(abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])), i, j});
-                }
-            }
+            graph.push_back({wt,{i,j}});
         }
-        
-        
-        while(edges!=n-1){
-            
-            vector<int> edge = pq.top();
-            
-            int p1 = getParent(parents, edge[1]);
-            int p2 = getParent(parents, edge[2]);
-            
-            if(p1!=p2){
-                
-                ans += -1*edge[0];
-                
-                parents[p1] = p2;
-                
-                edges++;
-                
-                
-            }
-            
-            pq.pop();
-            
-        }
-            
-        return ans;
-            
     }
+    sort(graph.begin(),graph.end());
+    int sum=0;
+    for(int i=0;i<graph.size();i++){
+        int s1=graph[i].second.first;
+        int s2=graph[i].second.second;
+        if(find(s1)!=find(s2)){
+            sum+=graph[i].first;
+            Union(find(s1),find(s2));
+        }
+    }
+    return sum;
+
+}
 };
