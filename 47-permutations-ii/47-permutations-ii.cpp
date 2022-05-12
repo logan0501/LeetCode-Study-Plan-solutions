d655e209d51ab7ad291a3a16deb79ecbe4df12e2
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-05-12 19:34:08 +0530

    Time: 20 ms (37.89%), Space: 11 MB (27.63%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-   void permuteUniqueHelper(int i, vector<int>& nums, vector<vector<int> >& result) {
-        if (i == nums.size()) { result.emplace_back(nums); return; }
+
+    void helper(int i,vector<int> &nums,vector<vector<int>> &res){
+        if(i==nums.size()){res.push_back(nums);return;}
         unordered_set<int> st;
-        for (int j = i; j < nums.size(); ++j) {
-            if (st.count(nums[j]) == 1) { continue; }
+        for(int j=i;j<nums.size();j++){
+            if(st.count(nums[j])==1)continue;
             st.insert(nums[j]);
-            std::swap(nums[i], nums[j]);
-            permuteUniqueHelper(i + 1, nums, result);
-            std::swap(nums[i], nums[j]);
+            swap(nums[i],nums[j]);
+            helper(i+1,nums,res);
+            swap(nums[i],nums[j]);
         }
     }
-    
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int> > result;
-        permuteUniqueHelper(0, nums, result);
-        return result;
+        vector<vector<int>> res;
+        helper(0,nums,res);
+        return res;
     }
 };
