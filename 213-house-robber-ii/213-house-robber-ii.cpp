8ed536b9d8f31fbf46aca6f05132437e2341a2fa
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-07-03 21:33:19 +0530

    Time: 3 ms (57.70%), Space: 8.1 MB (17.85%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-
-int helper(vector<int> &nums){
-    int dp[nums.size()+1];
-    dp[0]=nums[0];
-    dp[1]=max(nums[0],nums[1]);
-    for(int i=2;i<nums.size();i++){
-        dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
-    }
-    return dp[nums.size()-1];
+int houseRob(int i,vector<int> &nums,vector<int> &dp){
+    if(i<0)return 0;
+  
+    if(dp[i]!=-1)return dp[i];
+    if(i==0)return nums[0];
+    return dp[i]=max(nums[i]+houseRob(i-2,nums,dp),houseRob(i-1,nums,dp));
 }
-
 int rob(vector<int>& nums) {
-    if(nums.empty())return 0;
     if(nums.size()==1)return nums[0];
-    if(nums.size()==2)return max(nums[0],nums[1]);
-    vector<int> num1(nums.begin()+1,nums.end());
-    vector<int> num2(nums.begin(),nums.end()-1);
-    return max(helper(num1),helper(num2));
+    vector<int> dp1(nums.size(),-1),dp2(nums.size(),-1);
+
+    vector<int> v1(nums.begin(),nums.end()-1);
+    vector<int> v2(nums.begin()+1,nums.end());
+
+    return max(houseRob(v1.size()-1,v1,dp1),houseRob(v2.size()-1,v2,dp2));
 }
 };
