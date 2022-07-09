3c50040924f2a5528f92fa6a235ba22f74b50b44
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-07-09 09:26:39 +0530

    Time: 12 ms (95.26%), Space: 10.3 MB (50.61%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-vector<vector<int>> dp;
-int f(int i,int j,vector<vector<int>>& matrix){
-    if(j<0 || j>=matrix[0].size())return INT_MAX;
-    if(i==0)return matrix[i][j];   
-    if(dp[i][j]!=-1)return dp[i][j];
-    return dp[i][j]=matrix[i][j]+min(f(i-1,j-1,matrix),min(f(i-1,j,matrix),f(i-1,j+1,matrix)));
-}
 int minFallingPathSum(vector<vector<int>>& matrix) {
     int minv=INT_MAX;
     int m=matrix.size(),n=matrix[0].size();
-    dp.resize(m,vector<int>(n,-1));
-    for(int i=0;i<matrix[0].size();i++){
-       
-        minv=min(minv,f(matrix.size()-1,i,matrix));
+    vector<vector<int>> dp(m,vector<int>(n,0));
+    for(int i=0;i<n;i++)dp[0][i]=matrix[0][i];
+  
+    for(int i=1;i<m;i++){
+       for(int j=0;j<n;j++){
+         
+            dp[i][j]=matrix[i][j];
+            int ld=INT_MAX,rd=INT_MAX;
+            if(j>=1)ld=dp[i-1][j-1];
+            if(j+1<n)rd=dp[i-1][j+1];
+            dp[i][j]+=min(dp[i-1][j],min(ld,rd));
+       }
+        
+    }
+    for(int i=0;i<n;i++){
+        minv=min(dp[m-1][i],minv);
     }
     return minv;
 }
