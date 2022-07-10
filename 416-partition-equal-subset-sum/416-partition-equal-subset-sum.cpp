4ddfec799b4e3ffb7448ddd1f9b024a1934f7b05
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-07-10 19:45:26 +0530

    Time: 299 ms (51.33%), Space: 75.3 MB (27.22%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
 int helper(int n,int target,vector<int> &nums,vector<vector<int>> &dp){
-    if(nums[n]==target)return true;
+    if(target==0)return true;
     if(n==0)return nums[0]==target;
     if(dp[n][target]!=-1)return dp[n][target];
     int nottaken = helper(n-1,target,nums,dp);
     int taken = false;
-    if(nums[n]<target)taken = helper(n-1,target-nums[n],nums,dp);
+    if(nums[n]<=target)taken = helper(n-1,target-nums[n],nums,dp);
     return dp[n][target]= nottaken | taken;
 }
 bool canPartition(vector<int>& nums) {
