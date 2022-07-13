bf708210883adf30d12080b1e80697749f326cb8
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-07-13 23:32:46 +0530

    Time: 120 ms (60.61%), Space: 45.7 MB (19.43%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-    vector<int> dp(amount+1,0);
-    for(int i=1;i<=amount;i++){
-        dp[i]=INT_MAX;
-        for(auto c:coins){
-            if(i-c>=0 && dp[i-c]!=INT_MAX){
-                dp[i] = min(dp[i],dp[i-c]+1);
-            }
-        }
-    }     
-    return dp[amount]==INT_MAX?-1:dp[amount];   
+int f(int i,int amount,vector<int> &coins, vector<vector<int>> &dp){
+ 
+    if(i==0){
+        if(amount%coins[i]==0)return amount/coins[i];
+        else return 1e9;
     }
-};
+    if(dp[i][amount]!=-1)return dp[i][amount];
+    int notpick = f(i-1,amount,coins,dp);
+    int pick = 1e9;
+    if(amount>=coins[i])pick = 1+f(i,amount-coins[i],coins,dp);
+    return dp[i][amount]=min(notpick,pick);
+}
+int coinChange(vector<int>& coins, int amount) {
+    int n = coins.size();
+    vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+    int res= f(n-1,amount,coins,dp);
+    return res>=1e9?-1:res;
+}};
