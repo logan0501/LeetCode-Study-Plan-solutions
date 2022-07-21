2ed0ee9b66d239695ce106a96c1400eec956f9e1
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-07-21 18:49:35 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (50.82%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    bool isMatching(char c1,char c2){
-        return ((c1=='(' && c2==')') || (c1=='[' && c2==']') || (c1=='{' && c2=='}'));
+    bool check(char a,char b){
+        return (a=='(' && b==')') || (a=='{' && b=='}') || (a=='[' && b==']');
     }
     bool isValid(string s) {
         stack<char> st;
         for(char c:s){
-          
-            if(c=='(' || c=='[' || c=='{')st.push(c);
+            if(c=='(' || c=='{' || c=='[')st.push(c);
+        else{
+            if(st.empty())return false;
             else{
-                if(st.empty())return false;
-                else if(!isMatching(st.top(),c))return false;   
-                st.pop();
-            } 
-             
+         
+                if(check(st.top(),c)){
+                     
+                    st.pop();
+                }else return false;
+            }
         }
-        return st.empty()==true;
+        }
+        return st.empty();
+            
     }
+    
 };
