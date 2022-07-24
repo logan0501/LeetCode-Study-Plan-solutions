ea051fe60fb5c1b2e6680d507b9cfa2f6a1fb5b9
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-07-24 10:50:25 +0530

    Time: 8 ms (5.00%), Space: 6.6 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-vector<vector<int>> res;
-vector<int> f(int i){
-    if(i==1){
-        res.push_back({1});
-        return {1};
-    }
-    vector<int> prev=f(i-1);
-    vector<int> temp;
-   temp.push_back(1);
-    for(int i=1;i<prev.size();i++){
-        temp.push_back(prev[i]+prev[i-1]);
-    }
-    temp.push_back(1);
-    res.push_back(temp);
-    return temp;
-}
-vector<vector<int>> generate(int numRows) {
-     f(numRows);
-     return res;   
+
+vector<vector<int>> generate(int n) {
+     vector<vector<int>> res;
+    
+     vector<int> prev;
+     prev.push_back(1);
+     res.push_back(prev);
+     for(int i=0;i<n-1;i++){
+        vector<int> temp;
+        temp.push_back(1);
+        for(int j=1;j<prev.size();j++){
+            temp.push_back(prev[j]+prev[j-1]);
+        }
+        temp.push_back(1);
+         prev=temp;
+        res.push_back(temp);
+     }
+     return res;    
 }
 };
