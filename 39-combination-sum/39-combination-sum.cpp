1be8c3ad604483f984b1b796249518b9299a56c5
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-07-26 00:07:32 +0530

    Time: 15 ms (53.63%), Space: 11.3 MB (56.53%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
 vector<vector<int>> res;
-void f(int i,int sum,vector<int> temp,vector<int> &arr){
+void f(int i,int sum,vector<int> &temp,vector<int> &arr){
     if(i==arr.size()){
         if(sum==0){
             res.push_back(temp);            
         }
         return;
     }
-     f(i+1,sum,temp,arr);
     if(arr[i]<=sum){
         temp.push_back(arr[i]);
         f(i,sum-arr[i],temp,arr);
+        temp.pop_back();
     }
- 
+    f(i+1,sum,temp,arr);
 }
 vector<vector<int>> combinationSum(vector<int>& arr, int target) {
        vector<int> temp;
