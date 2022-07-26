2ae91cb7eb4969f7a17eecb0d9f1fd37f467f4cb
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-07-26 10:23:15 +0530

    Time: 9 ms (18.48%), Space: 15.1 MB (32.65%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-vector<vector<int>> res;
-void generate(vector<int> &v,int i,vector<int> &cur){
-	if(i==v.size()){
-		res.push_back(cur);
-		return;
-	}
-	cur.push_back(v[i]);
-	generate(v,i+1,cur);
-	cur.pop_back();
-	generate(v,i+1,cur);
-}
+    vector<vector<int>> res;
+    void f(int i,vector<int> nums,vector<int> &temp){
+        if(i==nums.size()){
+            res.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[i]);
+        f(i+1,nums,temp);
+        temp.pop_back();
+        f(i+1,nums,temp);
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> cur;
-        generate(nums,0,cur);
+        vector<int> temp;
+        f(0,nums,temp);
         return res;
     }
 };
