df1e7141abd26ea9c8346a4bf87d98dc3c04279c
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2022-07-28 07:51:46 +0530

    Time: 6 ms (90.64%), Space: 7.3 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    bool isAnagram(string s1, string s2) {
-     int arr[26]={0};
-        if(s1.length()!=s2.length()){
-            return false;
+    bool isAnagram(string s, string t) {
+        int hash[26]={0};
+        if(s.size()!=t.size())return false;
+        for(int i=0;i<s.size();i++){
+            hash[s[i]-'a']++;
+            hash[t[i]-'a']--;
         }
-    for(int i=0;i<s1.length();i++){
-        arr[s1[i]-'a']++;
-        arr[s2[i]-'a']--;
-    }
-    for(int count:arr){
-        if(count)return false;
-    }
-return true;
+        for(auto a:hash){
+            if(a>0)return false;
+        }
+        return true;
     }
 };
